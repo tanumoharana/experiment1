753e85f0b0de0faba863ac308b803b174de28828
tanumoharana <tanushreemoharana02@gmail.com>
2026-08-28 11:25:33 +0530

    Add alert and output message in script.js

---
+++ script.js
@@ -0,0 +1,2 @@
+alert("hello! the external javascript file is successfully linked.");
+document.getElementById("output_text").innerText="Success! The external JavaScript file is now Linked and running.";
